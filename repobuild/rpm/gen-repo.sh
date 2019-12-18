19be1f00983dcb0bb42aafa3fc94a40131644e70
e-kov <ikovalenko@assurio.com>
2019-12-18 19:25:46 +0200

    CentOS repository build improvements
    - Using fancy repo domain name now by https
    - Made separate builds for CentOS 6, 7, 8.

--- repobuild/rpm/gen-repo.sh
+++ repobuild/rpm/gen-repo.sh
@@ -9,10 +9,12 @@ die () {
 
 dir=$(readlink -f $(dirname $0))
 out=$dir/repo
-cent6=$out/CentOS/6
-pool=$cent6/x86_64/Packages
+dist_ver=`cat /etc/redhat-release | tr -cd [0-9.] | cut -d'.' -f1`
+cent=$out/CentOS/$dist_ver/x86_64
+pool=$cent/Packages
+mirrors=$out/CentOS/mirrors
 branch=`git rev-parse --abbrev-ref HEAD`
-bucket=ci.assur.io
+repo_link=https://repo.assur.io/assurio-snap
 
 [ -z "$GPG_KEY_ID" ]   && die "Env var GPG_KEY_ID is not set"
 [ -z "$GPG_PUB_KEY" ]  && die "Env var GPG_PUB_KEY is not set"
@@ -26,9 +28,9 @@ gpg2 --list-secret-key $GPG_KEY_ID >/dev/null || \
      die "Failed to import gpg key"
 
 # Cleanup and prepare dir structure
-rm -rf $out
-mkdir -p $cent6/x86_64/{Packages,repodata}
-mkdir -p $out/CentOS/mirrors
+rm -rf $cent $mirrors/$dist_ver*
+mkdir -p $cent/{Packages,repodata}
+mkdir -p $mirrors
 
 # Prepare 'Packages' dir. It's analog of the 'pool' dir for deb
 cp -r $dir/../../pkgbuild/RPMS/x86_64/* $pool
@@ -44,18 +46,16 @@ for f in `ls $pool/*.rpm`; do
 done
 
 # Generate repodata/repomd.xml
-createrepo $cent6/x86_64 || die "Failed to create repomd.xml"
+createrepo $cent || die "Failed to create repomd.xml"
 
 # Sign repository (repomd.xml files)
-gpg2 --default-key $GPG_KEY_ID --batch --yes --no-tty --armor --digest-algo SHA256 --detach-sign $cent6/x86_64/repodata/repomd.xml || \
+gpg2 --default-key $GPG_KEY_ID --batch --yes --no-tty --armor --digest-algo SHA256 --detach-sign $cent/repodata/repomd.xml || \
     die "Failed to sign repomd.xml file."
 
 # Generate 'mirrors'
-mirrors_content="http://$bucket.s3.amazonaws.com/repo/assurio-snap/$branch/rpm/CentOS/6/\$basearch"
-for ver in 6 7 8; do
-    for kind in '' 'Client' 'Server' 'Workstation'; do
-        echo $mirrors_content > $out/CentOS/mirrors/$ver$kind
-    done
+mirrors_content="$repo_link/$branch/rpm/CentOS/\$releasever/\$basearch"
+for kind in '' 'Client' 'Server' 'Workstation'; do
+    echo $mirrors_content > $mirrors/$dist_ver$kind
 done
 
 # Show repo's tree to the log
