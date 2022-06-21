76b9b3ad2cd8bb75ef938da33b1b0a4a7946f4df
e-kov <ikovalenko@assurio.com>
2022-06-21 19:31:13 +0300

    Implement tests on LVM mirrored device

--- tests/util.py
+++ tests/util.py
@@ -62,3 +62,63 @@ def mkfs(device, fs="ext4"):
         cmd = ["mkfs." + fs, "-F", device]
 
     subprocess.check_call(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=40)
+
+def dev_size_mb(device):
+    return int(subprocess.check_output("blockdev --getsize64 %s" % device, shell=True))//1024**2
+
+def assemble_mirror_lvm(devices, seed):
+    # 1. Create LVM partitions
+    partitions=[]
+    for device in devices:
+        cmd = ["parted", "--script", device, "mklabel gpt"]
+        subprocess.check_call(cmd, timeout=10)
+        cmd = ["parted", "--script", device, "mkpart 'LVM2' 0% 100%"]
+        subprocess.check_call(cmd, timeout=10)
+        cmd = ["parted", "--script", device, "set 1 lvm on"]
+        subprocess.check_call(cmd, timeout=10)
+
+        cmd = ["lsblk", device, "-l", "-o", "NAME", "-n"]
+        partitions.append("/dev/" + subprocess.check_output(cmd, timeout=10).rstrip().decode("utf-8").split("\n")[-1])
+
+    # 2. Create physical volume.  The command looks like 'pvcreate /dev/sdb1 /dev/sdc1'
+    cmd = ["pvcreate"]
+    cmd += partitions
+    subprocess.check_call(cmd, timeout=10)
+
+    # 3. Create volume group.  The command looks like 'vgcreate volgroup_mirror /dev/sdb1 /dev/sdc1'
+    vol_group = "vg_mirror" + str(seed)
+    logical_vol = "lv_mirror" + str(seed)
+    cmd = ["vgcreate", vol_group]
+    cmd += partitions
+    subprocess.check_call(cmd, timeout=10)
+
+    # 4. Create logical volume with mirroring.  The command looks like 'lvcreate -L 230MB -m1 -n testmirror_lv volgroup_mirror'
+    dev_size = dev_size_mb(devices[1])
+    log_vol_size = str(dev_size - int(dev_size/10)) + "MB"
+    cmd = ["lvcreate", "-L", log_vol_size, "-m1", "-n", logical_vol, vol_group]
+    subprocess.check_call(cmd, timeout=10)
+    lvm_dev = "/dev/" + vol_group + "/" + logical_vol
+    return lvm_dev
+
+
+def disassemble_mirror_lvm(lvm_device):
+    # 1. Disable LVM.  The command looks like 'lvchange -an /dev/vg_mirror22/lv_mirror22'
+    cmd = ["lvchange", "-an", lvm_device]
+    subprocess.check_call(cmd, timeout=10)
+
+    # 2. Delete LVM volume.  The command looks like 'lvremove /dev/vg_mirror22/lv_mirror22'
+    cmd = ["lvremove", "-f", lvm_device]
+    subprocess.check_call(cmd, timeout=10)
+
+    # 3. Disable volume group.  The command looks like 'vgremove vg_mirror22'
+    vol_group = lvm_device.replace("/dev/", "").split("/")[0]
+    cmd = ["vgremove", vol_group]
+    subprocess.check_call(cmd, timeout=10)
+
+
+def assemble_mirror_raid(devices):
+    return "not implemented"
+
+
+def disassemble_mirror_raid(raid_device):
+    return "not implemented"
