a34c3ff46707fc808bb464e19d021a2cadccc58a
Stanislav Barantsev <sbarantsev@elastio.com>
2022-07-21 21:38:49 +0300

    fixed copyright in feature-tests/bvec_iter_all.c

--- src/configure-tests/feature-tests/bvec_iter_all.c
+++ src/configure-tests/feature-tests/bvec_iter_all.c
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 /*
- * Copyright (C) 2015 Datto Inc.
- * Additional contributions by Elastio Software, Inc are Copyright (C) 2020 Elastio Software Inc.
+ * Copyright (C) 2020 Elastio Software Inc.
  */
 
 #include "includes.h"
