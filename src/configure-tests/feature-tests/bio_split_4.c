44f07bba5f5107e2bcf70b07765628a8bae80bca
Stanislav Barantsev <skypodolsky@gmail.com>
2022-11-07 18:12:40 +0200

    Update src/configure-tests/feature-tests/bio_split_4.c
    Co-authored-by: Eugene Kovalenko <ikovalenko@elastio.com>

--- src/configure-tests/feature-tests/bio_split_4.c
+++ src/configure-tests/feature-tests/bio_split_4.c
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 /*
- * Copyright (C) 2015 Datto Inc.
- * Additional contributions by Elastio Software, Inc are Copyright (C) 2020 Elastio Software Inc.
+ * Copyright (C) 2022 Elastio Software Inc.
  */
 
 // kernel version < 3.14
