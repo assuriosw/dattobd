eee0ac7e1420b28a9d9f29769457cf884e8f40f9
Konstantin Germanov <kgermanov@axcient.com>
2022-11-29 12:47:34 +0400

    Test flag COW_IN_BDEV

--- tests/test_storage_redirected.py
+++ tests/test_storage_redirected.py
@@ -25,34 +25,81 @@ class TestStorageRedirected(DeviceTestCaseMultipart):
         self.cow_file = "cow.snap"
         self.minor = self.minors[self.source_part_num]
         self.device = self.devices[self.source_part_num]
-        self.mount = self.mounts[self.target_part_num]
-        self.cow_full_path = "{}/{}".format(self.mount, self.cow_file)
+        self.source_mount = self.mounts[self.source_part_num]
+        self.target_mount = self.mounts[self.target_part_num]
+        self.cow_full_path = "{}/{}".format(self.target_mount, self.cow_file)
 
         self.snap_device = "/dev/elastio-snap{}".format(self.minor)
         self.snap_mount = "/tmp/elio-snap-mnt{0:03d}".format(self.minor)
         os.makedirs(self.snap_mount, exist_ok=True)
         self.addCleanup(os.rmdir, self.snap_mount)
 
-
-    def test_setup_and_destroy_redirected(self):
+    def test_redirected_setup_snapshot(self):
         self.assertEqual(elastio_snap.setup(self.minor, self.device, self.cow_full_path), 0)
+        self.addCleanup(elastio_snap.destroy, self.minor)
         self.assertTrue(os.path.exists(self.snap_device))
 
         snapdev = elastio_snap.info(self.minor)
         self.assertIsNotNone(snapdev)
+        self.assertEqual(snapdev["flags"], elastio_snap.Flags.COW_REDIRECTED)
+        self.assertEqual(snapdev["cow"], "/{}".format(self.cow_file))
 
         self.assertEqual(elastio_snap.destroy(self.minor), 0)
         self.assertFalse(os.path.exists(self.snap_device))
         self.assertIsNone(elastio_snap.info(self.minor))
         self.assertFalse(os.path.exists(self.cow_full_path))
 
-
-    def test_destroy_dormant(self):
+    def test_redirected_setup_incremental(self):
         self.assertEqual(elastio_snap.setup(self.minor, self.device, self.cow_full_path), 0)
-        util.unmount(self.mount)
+        self.addCleanup(elastio_snap.destroy, self.minor)
+        self.assertTrue(os.path.exists(self.snap_device))
 
-        #self.addCleanup(os.remove, self.cow_full_path)
-        self.addCleanup(util.mount, self.device, self.mount)
+        snapdev = elastio_snap.info(self.minor)
+        self.assertIsNotNone(snapdev)
+        self.assertEqual(snapdev["flags"], elastio_snap.Flags.COW_REDIRECTED)
+        self.assertEqual(snapdev["cow"], "/{}".format(self.cow_file))
+
+        self.assertEqual(elastio_snap.transition_to_incremental(self.minor), 0)
+        snapdev = elastio_snap.info(self.minor)
+        self.assertIsNotNone(snapdev)
+        self.assertEqual(snapdev["flags"], elastio_snap.Flags.COW_REDIRECTED)
+        self.assertEqual(snapdev["cow"], "/{}".format(self.cow_file))
+
+        new_cow_on_bdev = "{}/{}".format(self.source_mount, self.cow_file)
+        self.assertEqual(elastio_snap.transition_to_snapshot(self.minor, new_cow_on_bdev), 0)
+        snapdev = elastio_snap.info(self.minor)
+        self.assertIsNotNone(snapdev)
+        self.assertEqual(snapdev["flags"], elastio_snap.Flags.COW_ON_BDEV)
+        self.assertEqual(snapdev["cow"], "/{}".format(self.cow_file))
+
+
+    def test_redirected_modify_origin_snap(self):
+        pass
+
+    def test_redirected_reload_snapshot(self):
+        pass
+
+    def test_redirected_reload_incremental(self):
+        pass
+
+    def test_redirected_umount_target_snapshot(self):
+        pass
+
+    def test_redirected_umount_target_incremental(self):
+        pass
+
+    def test_umount_source_snapshot(self):
+        pass
+
+    def test_umount_source_incremental(self):
+        pass
+
+    def destroy_dormant(self):
+        self.assertEqual(elastio_snap.setup(self.minor, self.device, self.cow_full_path), 0)
+        util.unmount(self.source_mount)
+
+        self.addCleanup(os.remove, self.cow_full_path)
+        self.addCleanup(util.mount, self.device, self.source_mount)
 
         self.assertEqual(elastio_snap.destroy(self.minor), 0)
         self.assertFalse(os.path.exists(self.snap_device))
