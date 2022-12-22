732eaba9a15997bdfd2845d98d69c249a2ccbc84
Stanislav Barantsev <skypodolsky@gmail.com>
2022-12-22 15:33:46 +0200

    updated header

--- src/configure-tests/feature-tests/write_zeroes.c
+++ src/configure-tests/feature-tests/write_zeroes.c
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 /*
- * Copyright (C) 2017 Datto Inc.
- * Additional contributions by Elastio Software, Inc are Copyright (C) 2020 Elastio Software Inc.
+ * Copyright (C) 2022 Elastio Software Inc.
  */
 
 // kernel_version >= 4.10
